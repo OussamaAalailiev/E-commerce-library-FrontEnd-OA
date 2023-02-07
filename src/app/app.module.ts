72ffc78081bb86c4c66cb9f8905324a25f33ef8d
Oussama Aalaila <aalaila.oussama@gmail.com>
2023-02-07 18:30:10 +0100

    1st step into project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { MainContentsComponent } from './components/main-contents/main-contents.component';
+import { FooterComponent } from './components/footer/footer.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainContentsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {path: '', component: MainContentsComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
