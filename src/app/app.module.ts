618e8c3df28bcd088b8cdb83d4d91cc29c937fd9
Oussama Aalaila <aalaila.oussama@gmail.com>
2023-02-10 22:35:40 +0100

    UI Modify of 2nd Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { HeaderComponent } from './components/header/header.component';
 import { MainContentsComponent } from './components/main-contents/main-contents.component';
 import { FooterComponent } from './components/footer/footer.component';
 import {RouterModule} from "@angular/router";
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+
 
 @NgModule({
   declarations: [
@@ -22,7 +26,10 @@ import {RouterModule} from "@angular/router";
     BrowserAnimationsModule,
     RouterModule.forRoot([
       {path: '', component: MainContentsComponent}
-    ])
+    ]),
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
